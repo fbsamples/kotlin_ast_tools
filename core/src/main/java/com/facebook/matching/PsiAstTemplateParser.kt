834a95d02679b7eae460e90c860db0041c5b830a
Omer Strulovich <ostrulovich@meta.com>
2024-03-07 11:22:28 -0800

    Move main template parsing methods to their own class
    Summary: These will need to be an an object to get some dependencies. First let's move them to their own file.
    
    Differential Revision: D54600443
    
    fbshipit-source-id: 853185e1bfb2c989fd350b8666325c563cc4bb19

---
+++ core/src/main/java/com/facebook/matching/PsiAstTemplateParser.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.matching
+
+import org.jetbrains.kotlin.psi.KtElement
+
+/**
+ * Takes a template string and an optional list of matchers per variavle and builds a
+ * [PsiAstMatcher] for that template
+ */
+inline fun <reified T : Any> parseTemplateWithVariables(
+    template: String,
+    vararg variables: Pair<String, PsiAstMatcher<*>>
+): PsiAstMatcher<T> {
+  val unusedVariables = variables.toMap(mutableMapOf())
+
+  val rangesToTemplateVariables: List<Pair<IntRange, Variable>> =
+      Variable.TEMPLATE_VARIABLE_REGEX.findAll(template)
+          .map { v ->
+            v.range to
+                Variable(
+                    checkNotNull(v.groups["name"]).value,
+                    unusedVariables.remove(v.value) ?: Variable.ANY_SENTINEL,
+                    isOptional = checkNotNull(v.groups["isOptional"]).value == "?",
+                    isKotlin = KtElement::class.java.isAssignableFrom(T::class.java),
+                    arguments = v.groups["arguments"]?.value)
+          }
+          .toList()
+  val templateVariables = rangesToTemplateVariables.map { it.second }
+  check(unusedVariables.isEmpty()) {
+    "The following variables were not found in the template: " +
+        unusedVariables.keys.joinToString(separator = ", ") +
+        "\nVariables found in template: " +
+        templateVariables.joinToString { it.templateString }
+  }
+
+  check(templateVariables.map { it.name }.toSet().size == templateVariables.size) {
+    "Multiple reference to the same template variable are not supported yet"
+  }
+
+  val newTemplate =
+      rangesToTemplateVariables.reversed().fold(template) { template, variable ->
+        template.replaceRange(variable.first, variable.second.parsableCodeString)
+      }
+  return PsiAstTemplate(templateVariables).parse(T::class.java, newTemplate)
+}
+
+fun parseReplacementTemplate(
+    template: String,
+    replacement: String,
+    templateVariablesToText: Map<String, String>,
+): String {
+  var processedReplacment = replacement
+  "#[A-Za-z0-9_]+#".toRegex().findAll(template).forEach { matchResult ->
+    val target = matchResult.value
+    val variableValue =
+        templateVariablesToText[matchResult.value.removeSurrounding("#").removeSuffix("?")]
+            ?: error(
+                "undeclared variable ${matchResult.value}, known variables: ${templateVariablesToText.keys}")
+    processedReplacment = processedReplacment.replace(target, variableValue)
+  }
+  return processedReplacment
+}
